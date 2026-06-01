d7dce617aca51143cdfc80ad64f778db3813634f
justin212553 <wonseuk2125@gmail.com>
2026-05-31 22:40:26 -0700

    eval preprocess log name

--- scripts/preprocess_eval.sh
+++ scripts/preprocess_eval.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=16         # ★핵심: 파이썬 코드의 max_workers와 동일하게 16코어 징집
 #SBATCH --mem=64G                  # 대용량 이미지 버퍼 핸들링을 위해 64GB 탑재
 #SBATCH --time=12:00:00            # 충분히 넉넉하게 12시간 요새 방어
-#SBATCH --output=/pub/wonseukl/Path-ViT/.logs/preprocess_progress.log
+#SBATCH --output=/pub/wonseukl/Path-ViT/.logs/preprocess_progress_eval.log
 
 cd /pub/wonseukl/Path-ViT/
 source .venv/bin/activate
