0b7630548e64fdd32ec08c42419d389707b958c9
justin212553 <wonseuk2125@gmail.com>
2026-06-01 23:04:48 -0700

    only A30

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=free-gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:A30:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G                  
 #SBATCH --time=18:00:00            
