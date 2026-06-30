5fd2989e3a8dc1da6b189acc95be666b5769fb62
justin212553 <wonseuk2125@gmail.com>
2026-06-30 11:54:00 -0700

    download slurm job opt

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=standard
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --time=24:00:00
 #SBATCH --output=/pub/wonseukl/Path-ViT/.logs/download_progress.log
