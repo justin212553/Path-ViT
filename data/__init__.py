5f8fce1be92264b4a1fdfae5c7a27af4e4cd9d6f
Justin Lee <74852324+justin212553@users.noreply.github.com>
2026-07-02 23:21:02 -0700

    Add CAMELYON16SlideDataset to __all__ exports

--- data/__init__.py
+++ data/__init__.py
@@ -1,3 +1,4 @@
 from .patch_dataset import CAMELYON17NodeDataset
+from .camelyon16_dataset import CAMELYON16SlideDataset
 
-__all__ = ["CAMELYON17NodeDataset"]
+__all__ = ["CAMELYON17NodeDataset", "CAMELYON16SlideDataset"]
