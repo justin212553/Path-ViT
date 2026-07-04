9ed8c86c850cae0299bb88f61946dee474fe9d72
justin212553 <wonseuk2125@gmail.com>
2026-07-03 23:58:48 -0700

    decrease time

--- scripts/download_camelyon16_mil.sh
+++ scripts/download_camelyon16_mil.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --time=24:00:00
+#SBATCH --time=2:00:00
 #SBATCH --output=/pub/wonseukl/Path-ViT/.logs/camelyon16_mil_download_%j.log
 
 cd /pub/wonseukl/Path-ViT/
