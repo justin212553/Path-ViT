eaab4da9d25521327fb51e5d35de9a6810ce8751
justin212553 <wonseuk2125@gmail.com>
2026-07-13 18:27:28 -0700

    preprocess oom fix

--- scripts/preprocess_cptac.sh
+++ scripts/preprocess_cptac.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:A30:1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 #SBATCH --time=3-00:00:00
 #SBATCH --output=/pub/wonseukl/Path-ViT/.logs/preprocess_cptac.log
 
